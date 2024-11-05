64b9349acb6172194ea899040e4007688237a5cb
Bujar Raufi <bujar.raufi@tudublin.ie>
2024-11-05 15:25:58 +0200

    Initial Commit

---
+++ webmap/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Apply database migrations
+echo "Applying database migrations..."
+python manage.py migrate
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --no-input
+
+# Execute the command passed to the container
+exec "$@"
